6d2f0a16307ea134add4247da528e60966eb64cc
mouhssine007 <mouhssinemajdi7@gmail.com>
2024-01-23 13:48:18 +0100

    Update main.js
    prime number

--- main.js
+++ main.js
@@ -1,64 +1,26 @@
-const checkbox = document.getElementById("checkbox");
-const submitBtn=document.querySelector("button[type=submit]");
-//disable checkbox
-checkbox.disabled=true;
-submitBtn.disabled=true;
+//Promptse user  for the interval 
+let startNumber = parseInt(prompt(" Enter the starting number of the interval"));
+let endNumber = parseInt(prompt(" Enter the ending number of the interval"));
+//check if inputs are valid integer 
 
+if(!isNaN(startNumber)&& !isNaN(endNumber) && Number.isInteger(startNumber)&& Number.isInteger(endNumber) && startNumber<endNumber && startNumber>1){
+    console.log(`Prime numbers in the interval ${startNumber}, ${endNumber} :`);
+    //chedk for prime numbers in the interbal 
+    for(let i=startNumber ;i<=endNumber;i++){
+  
+        let isPrime = true ;
 
-const elements=document.querySelectorAll(".element");
-const selectColor=document.getElementById("selectorColor");
-//selectColor = document.querySelector("#selectorColor");
+        for(j=2; j<=Math.sqrt(i);j++){
+            if(i%j === 0){
+                isPrime=false;
+                break;
+            }
+        }
 
-console.log(selectColor)
-
-elements.forEach(function(element){
-    const color= getRandomColor();
-    element.style.backgroundColor=color;
-    element.innerHTML=color;
-    selectColor.innerHTML=color;
-});
-
-function getRandomColor(){
-    const letter = "0123456789ABCDEF";
-    let color = "#";
-    for(let i = 0; i<6;i++){
-        color += letter[Math.floor(Math.random() * 16)];
-    
+        if(isPrime){
+            console.log(i);
+        }
     }
-    return color; 
-    }
-
-    //check if right color 
-
-
-elements.forEach(function(element){
-element.addEventListener('click' , function(){
-    if(element.innerHTML===selectColor.innerHTML){
-        checkbox.checked=true;
-        submitBtn.disabled=false;
-        submitBtn.classList.remove("btn-light");
-        submitBtn.classList.add("btn-success");
-    }else{
-        submitBtn.classList.remove("btn-light");
-
-        alert("please verify if you are human");
-        location.reload(true);
-
-    }
-})
-});
-
-
-//prompt user for the range 
-let minRange = parseFloat(prompt("Enter the minimum value of the rang : "));
-let maxRange = parseFloat(prompt("Enter the maximum value of the rang : "));
-//check if input is a valid number
-
-if(!isNaN(minRange) && !isNaN(maxRange) && minRange<maxRange){
-    //generate a random number within the  specified range
-    let randomNumber = Math.random()*(maxRange - minRange) + minRange;
-    console.log(`A random number between ${minRange} and ${maxRange} is ${randomNumber}`);
-}else{
-    console.log("Please enter valid number , ensuring that the minimum value is less than the maximum value.");
-
+}else {
+    console.log("Please enter valid integer ,ensuring that the starting number is less than the eneding number and greater than1");
 }
